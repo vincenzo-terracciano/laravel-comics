4f207d13670033bf3fa46f679d8cc05d13d4cbcb
vincenzo-terracciano <vincenzo.terracciano97@gmail.com>
2025-06-17 16:23:38 +0200

    Refactor home view to use partials for jumbotron and shop link sections

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,7 +2,7 @@
 
 @section('comics')
 
-    <div class="jumbotron"></div>
+    @include('partials.jumbotron')
 
     <section id="current-series" class="dark">
         <div class="container">
@@ -32,29 +32,6 @@
             </div>
         </div>
 
-        <section class="shop-link py-5">
-            <div class="container d-flex justify-content-center gap-5 align-items-center">
-                <div class="card-shop d-flex gap-3 align-items-center">
-                    <img src="{{ Vite::asset('public/img/buy-comics-digital-comics.png') }}" alt="Digital Comics">
-                    <a class="uppercase" href="#">digital comics</a>
-                </div>
-                <div class="card-shop d-flex gap-3 align-items-center">
-                    <img src="{{ Vite::asset('public/img/buy-comics-merchandise.png') }}" alt="DC Merchandise">
-                    <a class="uppercase" href="#">dc merchandise</a>
-                </div>
-                <div class="card-shop d-flex gap-3 align-items-center">
-                    <img src="{{ Vite::asset('public/img/buy-comics-subscriptions.png') }}" alt="Subscription">
-                    <a class="uppercase" href="#">subscription</a>
-                </div>
-                <div class="card-shop d-flex gap-3 align-items-center">
-                    <img class="locator" src="{{ Vite::asset('public/img/buy-comics-shop-locator.png') }}" alt="Comic Shop Locator">
-                    <a class="uppercase" href="#">comic shop locator</a>
-                </div>
-                <div class="card-shop d-flex gap-3 align-items-center">
-                    <img src="{{ Vite::asset('public/img/buy-dc-power-visa.svg') }}" alt="DC Power Visa">
-                    <a class="uppercase" href="#">dc power visa</a>
-                </div>
-            </div>
-        </section>
+        @include('partials.shop-link')
     </section>
 @endsection
